7cf26ab26eb1fc755f29029c9e6c4c442b592848
Hasnain Khan🏴 <official.thevm@gmail.com>
2025-04-29 10:13:20 +0500

    done

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <title>FREELEVANCE - IT Agency</title>
+      <title>
+        𝗙©𝟮𝟱 — Transforming Ideas into Reality | Web & App Development |
+        Automation & AI Solutions
+      </title>
       <Head />
       <body>
         <Main />
